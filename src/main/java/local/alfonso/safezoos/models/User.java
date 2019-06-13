349cc881b50fee2d6d440bf751c3b4f02da02403
Alfonso <alfonso.garcia2324@gmail.com>
2019-06-13 14:23:54 -0700

    Finished User

--- src/main/java/local/alfonso/safezoos/models/User.java
+++ src/main/java/local/alfonso/safezoos/models/User.java
@@ -1,10 +1,21 @@
 package local.alfonso.safezoos.models;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import javax.persistence.*;
 
 @Entity
 @Table(name = "users")
 public class User extends Auditable
 {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long userid;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private String password;
 }
