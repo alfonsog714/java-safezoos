fe8fb80b4ca05606ae9c2fadc8699e2ed2d5d451
Alfonso <alfonso.garcia2324@gmail.com>
2019-06-13 14:40:13 -0700

    Added getters and setters to UseR

--- src/main/java/local/alfonso/safezoos/models/User.java
+++ src/main/java/local/alfonso/safezoos/models/User.java
@@ -1,8 +1,15 @@
 package local.alfonso.safezoos.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+// User is the parent Entity
 
 @Entity
 @Table(name = "users")
@@ -18,4 +25,82 @@ public class User extends Auditable
     @Column(nullable = false)
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String password;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    @JsonIgnoreProperties("user")
+    private List<UserRoles> userRoles = new ArrayList<>();
+
+    public User()
+    {
+    }
+
+    public User(String username, String password, List<UserRoles> userRoles)
+    {
+        setUsername(username);
+        setPassword(password);
+        for (UserRoles ur : userRoles)
+        {
+            ur.setUser(this);
+        }
+        this.userRoles = userRoles;
+    }
+
+    public long getUserid()
+    {
+        return userid;
+    }
+
+    public void setUserid(long userid)
+    {
+        this.userid = userid;
+    }
+
+    public String getUsername()
+    {
+        return username;
+    }
+
+    public void setUsername(String username)
+    {
+        this.username = username;
+    }
+
+    public String getPassword()
+    {
+        return password;
+    }
+
+    public void setPassword(String password)
+    {
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        this.password = passwordEncoder.encode(password);
+    }
+
+    public void setPasswordNoEncrpyt(String password)
+    {
+        this.password = password;
+    }
+
+    public List<UserRoles> getUserRole()
+    {
+        return userRoles;
+    }
+
+    public void setUserRole(List<UserRoles> userRole)
+    {
+        this.userRoles = userRole;
+    }
+
+    public List<SimpleGrantedAuthority> getAuthority()
+    {
+        List<SimpleGrantedAuthority> rtnList = new ArrayList<>();
+
+        for (UserRoles r : this.userRoles)
+        {
+            String myRole = "ROLE_" + r.getRole().getName().toUpperCase();
+            rtnList.add(new SimpleGrantedAuthority(myRole));
+        }
+
+        return rtnList;
+    }
 }
